3778b4d0f55948b6d67c45904d9bbeca3f2d412b
huangxiaoan <huangxiaoan@wps.cn>
2023-07-19 07:10:53 +0800

    dev

--- main.go
+++ main.go
@@ -8,10 +8,9 @@ import (
 )
 
 func main() {
-	var files []string
+	var files, other []string
 
-	root := "D:/中医书籍/中医书籍"
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	filepath.Walk("D:/中医书籍/中医书籍", func(path string, info os.FileInfo, err error) error {
 		if info.Mode()&os.ModeDir > 0 {
 			return nil
 		}
@@ -27,9 +26,23 @@ func main() {
 		files = append(files, info.Name()+" "+sizeText)
 		return nil
 	})
-	if err != nil {
-		panic(err)
-	}
+
+	filepath.Walk("D:/中医书籍/其他电子书", func(path string, info os.FileInfo, err error) error {
+		if info.Mode()&os.ModeDir > 0 {
+			return nil
+		}
+
+		size := info.Size()
+		size = size / 1024
+		sizeText := fmt.Sprintf("%dk", size)
+		if size > 1024 {
+			size = size / 1024
+			sizeText = fmt.Sprintf("%dM", size)
+		}
+
+		other = append(other, info.Name()+" "+sizeText)
+		return nil
+	})
 
 	os.WriteFile("README.md", []byte(
 		"## 中医资料大全，持续更新"+
@@ -37,6 +50,8 @@ func main() {
 			strings.Join(files, "\r\n")+
 			"\n````\n"+
 			"共"+fmt.Sprintf("%d", len(files))+"本, 持续更新\r\n"+
-			"### 加微信获取\n![alt 属性文本](w.png)"),
+			"### 加微信获取\n![alt 属性文本](w.png)\n## 其他电子书\n````\n"+
+			strings.Join(other, "\n")+
+			"\n````\n"),
 		0777)
 }
